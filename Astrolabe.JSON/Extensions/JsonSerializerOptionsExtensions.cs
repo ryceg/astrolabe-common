ab52ae5ba50a0f3fb50839f0353b93a5ed39587e
Jolse Maginnis <doolse@gmail.com>
2023-09-17 17:50:47 +1000

    Case insensitive properties for the db

--- Astrolabe.JSON/Extensions/JsonSerializerOptionsExtensions.cs
+++ Astrolabe.JSON/Extensions/JsonSerializerOptionsExtensions.cs
@@ -8,6 +8,7 @@ public static class JsonSerializerOptionsExtensions
     public static JsonSerializerOptions AddStandardOptions(this JsonSerializerOptions options)
     {
         options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
         options.Converters.Add(new JsonBaseTypeConverter());
         options.Converters.Add(new StringAttributeConverter());
         return options;
