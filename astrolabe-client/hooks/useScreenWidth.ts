80703256f18659ab042c7ce2f58589798c96eea4
Jolse Maginnis <doolse@gmail.com>
2023-10-18 09:24:42 +1100

    Make useScreenWidth server side friendly

--- astrolabe-client/hooks/useScreenWidth.ts
+++ astrolabe-client/hooks/useScreenWidth.ts
@@ -2,17 +2,18 @@ import { useControl } from "@react-typed-forms/core";
 import { useEffect } from "react";
 
 export function useScreenWidth() {
-	const screenWidth = useControl(window.innerWidth);
-	useEffect(() => {
-		const handleResize = () => {
-			screenWidth.value = window.innerWidth;
-		};
-		window.addEventListener("resize", handleResize);
-		handleResize();
-		// Remove event listener on cleanup
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
-	return screenWidth;
+  if (typeof window === "undefined") return useControl(1024);
+  const screenWidth = useControl(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      screenWidth.value = window.innerWidth;
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    // Remove event listener on cleanup
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return screenWidth;
 }
