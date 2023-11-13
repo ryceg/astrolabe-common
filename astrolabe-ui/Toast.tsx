cea1760b280f6bd73911381215b2805e5344d88e
Rhys Gray <rhys.gray@itechnologies.com.au>
2023-11-13 12:49:07 +1100

    fix contrast, resize on smaller screens

--- astrolabe-ui/Toast.tsx
+++ astrolabe-ui/Toast.tsx
@@ -7,16 +7,21 @@ import {
 	useControl,
 } from "@react-typed-forms/core";
 import * as ToastPrim from "@radix-ui/react-toast";
-import { Toast, ToastService, ToastSettings } from "@astrolabe/client/service/toast";
+import {
+	Toast,
+	ToastService,
+	ToastSettings,
+} from "@astrolabe/client/service/toast";
 import { AppProvider } from "@astrolabe/client/service";
 import { cva } from "class-variance-authority";
 import { Button } from "./Button";
+import { cn } from "@astrolabe/client/util/utils";
 
 type ToastState = Toast[];
 
 export function useToastsRenderer(): [
 	ToastService,
-	AppProvider<ToastPrim.ToastProviderProps>
+	AppProvider<ToastPrim.ToastProviderProps>,
 ] {
 	const state = useControl<ToastState>([]);
 
@@ -30,7 +35,7 @@ export function useToastsRenderer(): [
 				{children}
 				<ToastPrim.Viewport
 					className={
-						"fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:right-0 sm:top-0 sm:flex-col md:max-w-[420px]"
+						"fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse gap-1 sm:p-4 p-2 sm:right-0 sm:flex-col max-w-md"
 					}
 				/>
 				<RenderToasts control={state} />
@@ -42,14 +47,17 @@ export function useToastsRenderer(): [
 }
 
 const toastVariants = cva(
-	"group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-auto rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-bottom-full data-[state=open]:sm:slide-in-from-top-full",
+	cn(
+		"group pointer-events-auto relative flex h-fit w-full items-center justify-between space-x-4 overflow-auto rounded-md border p-6 pr-8 shadow-lg",
+		"transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-bottom-full data-[state=open]:sm:slide-in-from-top-full"
+	),
 	{
 		variants: {
 			variant: {
-				info: "bg-blue-500 text-white border-blue-800",
-				warning: "bg-warning-500 text-warning-950 border-warning-800",
-				success: "bg-success-500 text-success-950 border-success-800",
-				error: "bg-danger-500 text-danger-950 border-danger-800",
+				info: "bg-blue-200 text-blue-950 border-blue-800",
+				warning: "bg-warning-200 text-warning-950 border-warning-800",
+				success: "bg-success-200 text-success-950 border-success-800",
+				error: "bg-danger-200 text-danger-950 border-danger-800",
 			},
 		},
 		defaultVariants: {
