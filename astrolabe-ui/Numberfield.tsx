e06974e8de54ab0e9ed84a02c642b39704a94353
Jolse Maginnis <doolse@gmail.com>
2023-12-06 10:31:13 +1100

    Support input props

--- astrolabe-ui/Numberfield.tsx
+++ astrolabe-ui/Numberfield.tsx
@@ -1,57 +1,70 @@
-import { Control, RenderForm, useControlEffect } from "@react-typed-forms/core";
-import { useState } from "react";
+import {
+  Control,
+  formControlProps,
+  RenderForm,
+  useControlEffect,
+} from "@react-typed-forms/core";
+import { InputHTMLAttributes, useState } from "react";
 import clsx from "clsx";
 
+export interface NumberfieldProps
+  extends InputHTMLAttributes<HTMLInputElement> {
+  control: Control<number | null | undefined>;
+  label: string;
+  required?: boolean;
+  inputClass?: string;
+}
+
 export function Numberfield({
-	control,
-	label,
-	required = false,
-	className,
-}: {
-	control: Control<number | null | undefined>;
-	label: string;
-	required?: boolean;
-	className?: string;
-}) {
-	const id = "c" + control.uniqueId;
+  control,
+  label,
+  required = false,
+  className,
+  inputClass,
+  ...inpProps
+}: NumberfieldProps) {
+  const id = "c" + control.uniqueId;
 
-	const [field, setField] = useState(makeTextAndValue(control.current.value));
+  const [field, setField] = useState(makeTextAndValue(control.current.value));
 
-	useControlEffect(
-		() => control.value,
-		(v) => setField((fv) => (fv[1] === v ? fv : makeTextAndValue(v)))
-	);
+  useControlEffect(
+    () => control.value,
+    (v) => setField((fv) => (fv[1] === v ? fv : makeTextAndValue(v))),
+  );
 
-	return (
-		<div className={clsx("flex flex-col", className)}>
-			<label htmlFor={id} className="font-bold">
-				{label}
-				{required ? " *" : ""}
-			</label>
-			<RenderForm
-				control={control}
-				children={({ errorText, value, ...props }) => (
-					<input
-						type="number"
-						className="input-field"
-						{...props}
-						value={field[0]}
-						onChange={(e) => {
-							const textValue = e.target.value;
-							const v = parseFloat(textValue);
-							const newValue = isNaN(v) ? null : v;
-							setField([textValue, v]);
-							control.value = newValue;
-						}}
-					/>
-				)}
-			/>
-		</div>
-	);
+  const { value, errorText, ...props } = formControlProps(control);
 
-	function makeTextAndValue(
-		value?: number | null
-	): [string | number, number | null | undefined] {
-		return [typeof value === "number" ? value : "", value];
-	}
+  return (
+    <div className={clsx("flex flex-col", className)}>
+      <label htmlFor={id} className="font-bold">
+        {label}
+        {required ? " *" : ""}
+      </label>
+      <input
+        type="number"
+        {...props}
+        className={inputClass}
+        value={field[0]}
+        onChange={(e) => {
+          const textValue = e.target.value;
+          const v = parseFloat(textValue);
+          const newValue = isNaN(v) ? null : v;
+          setField([textValue, v]);
+          control.value = newValue;
+        }}
+        {...inpProps}
+      />
+      {errorText && (
+        <p className="mt-2 text-sm text-danger-600 dark:text-danger-500">
+          {errorText}
+        </p>
+      )}
+    </div>
+  );
+
+  function makeTextAndValue(
+    value?: number | null,
+  ): [string | number, number | null | undefined] {
+    return [typeof value === "number" ? value : "", value];
+  }
 }
