97903d70350ce68ffe97692e3f69806ed5902734
Jolse Maginnis <doolse@gmail.com>
2024-06-17 18:14:02 +1000

    Make the methods public

--- Astrolabe.Workflow/AbstractWorkflowExecutor.cs
+++ Astrolabe.Workflow/AbstractWorkflowExecutor.cs
@@ -3,9 +3,9 @@ namespace Astrolabe.Workflow;
 public abstract class AbstractWorkflowExecutor<TContext, TLoadContext, TAction> 
     where TContext : IWorkflowActionList<TContext, TAction>
 {
-    protected abstract Task<IEnumerable<TContext>> LoadData(TLoadContext loadContext);
+    public abstract Task<IEnumerable<TContext>> LoadData(TLoadContext loadContext);
 
-    protected async Task<TContext> ApplyChanges(TContext context)
+    public async Task<TContext> ApplyChanges(TContext context)
     {
         context = await context.PerformActions(PerformAction);
         return await AfterActions(context);
