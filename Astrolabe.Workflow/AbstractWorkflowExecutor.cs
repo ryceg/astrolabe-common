ca6c9c33759d70d25e04e49598c10ce204ab4c91
Jolse Maginnis <doolse@gmail.com>
2024-06-17 16:59:26 +1000

    At least do the perform actions

--- Astrolabe.Workflow/AbstractWorkflowExecutor.cs
+++ Astrolabe.Workflow/AbstractWorkflowExecutor.cs
@@ -1,10 +1,20 @@
 namespace Astrolabe.Workflow;
 
-public abstract class AbstractWorkflowExecutor<TContext, TLoadContext, TAction>
+public abstract class AbstractWorkflowExecutor<TContext, TLoadContext, TAction> 
+    where TContext : IWorkflowActionList<TContext, TAction>
 {
     protected abstract Task<IEnumerable<TContext>> LoadData(TLoadContext loadContext);
 
-    protected abstract Task ApplyChanges(TContext context);
+    protected async Task<TContext> ApplyChanges(TContext context)
+    {
+        context = await context.PerformActions(PerformAction);
+        return await AfterActions(context);
+    }
+
+    protected virtual Task<TContext> AfterActions(TContext context)
+    {
+        return Task.FromResult(context);
+    }
 
     protected abstract Task<TContext> PerformAction(TContext context, TAction action);
 }
